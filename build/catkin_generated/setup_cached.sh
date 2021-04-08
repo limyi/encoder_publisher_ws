c049e0b7dccf56400268681d19d2e29676470770
jcch96 <joel_chan@sutd.edu.sg>
2021-04-08 11:15:34 +0800

    marker function

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,11 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/sutd/encoder_publisher_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/sutd/encoder_publisher_ws/devel/lib:/opt/ros/melodic/lib:/usr/local/cuda-10.0/lib64:/usr/local/cuda-10.0/lib64:/usr/local/cuda-10.0/lib64'
+export PATH='/opt/ros/melodic/bin:/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/bin:/home/sutd/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/sutd/encoder_publisher_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/sutd/encoder_publisher_ws/build'
+export PYTHONPATH="/home/sutd/encoder_publisher_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/sutd/encoder_publisher_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/sutd/encoder_publisher_ws/src:$ROS_PACKAGE_PATH"
