c073f3be04a6036f0efa8a8b0e71f56df6307350
jcch96 <joelchanch@gmail.com>
2021-05-12 14:48:12 +0800

    fixed ds4 driver

--- devel/lib/python2.7/dist-packages/ds4_driver/msg/_Status.py
+++ devel/lib/python2.7/dist-packages/ds4_driver/msg/_Status.py
@@ -306,7 +306,8 @@ float32 y       # 0.0: left edge, 1.0: right edge
     unpack serialized message in str into this message instance
     :param str: byte array of serialized message, ``str``
     """
-    codecs.lookup_error("rosmsg").msg_type = self._type
+    if python3:
+      codecs.lookup_error("rosmsg").msg_type = self._type
     try:
       if self.header is None:
         self.header = std_msgs.msg.Header()
@@ -416,7 +417,8 @@ float32 y       # 0.0: left edge, 1.0: right edge
     :param str: byte array of serialized message, ``str``
     :param numpy: numpy python module
     """
-    codecs.lookup_error("rosmsg").msg_type = self._type
+    if python3:
+      codecs.lookup_error("rosmsg").msg_type = self._type
     try:
       if self.header is None:
         self.header = std_msgs.msg.Header()
